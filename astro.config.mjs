ebb527626e9ef699403d84a8405ac4d732ce0d86
Stephen Lee <smlee.981@gmail.com>
2025-08-16 11:04:55 -0500

    add hero and get ready for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  site: 'https://thecvguide.com',
+  // Remove base since you'll use root domain
+});
